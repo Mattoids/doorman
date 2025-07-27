4f6456264d5438075738843e07390d01370cdbac
mattoid <liufei@liufei.info>
2025-07-27 22:03:12 +0800

    修改每页显示10条

--- js/src/admin/components/DoorkeyListPage.tsx
+++ js/src/admin/components/DoorkeyListPage.tsx
@@ -37,7 +37,7 @@ export default class DoorkeyListPage extends ExtensionPage {
   /**
    * Number of doorkeys to load per page.
    */
-  private numPerPage: number = 50;
+  private numPerPage: number = 10;
 
   /**
    * Current page number. Zero-indexed.
